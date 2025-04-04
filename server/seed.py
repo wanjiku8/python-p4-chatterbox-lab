e1621ed2ccafa7b59cf78f65a6468c4e2867325b
wanjiku8 <wanjikuf141@gmail.com>
2025-04-04 00:52:09 -0700

    commit

--- server/seed.py
+++ server/seed.py
@@ -1,34 +1,62 @@
-#!/usr/bin/env python3
-
-from random import choice as rc
-
-from faker import Faker
-
-from app import app
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from models import db, Message
 
-fake = Faker()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-usernames = [fake.first_name() for i in range(4)]
-if "Duane" not in usernames:
-    usernames.append("Duane")
+CORS(app)
+db.init_app(app)
 
-def make_messages():
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([{
+        'id': message.id,
+        'body': message.body,
+        'username': message.username,
+        'created_at': message.created_at,
+        'updated_at': message.updated_at
+    } for message in messages])
 
-    Message.query.delete()
-    
-    messages = []
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    message = Message(
+        body=data['body'],
+        username=data['username']
+    )
+    db.session.add(message)
+    db.session.commit()
+    return jsonify({
+        'id': message.id,
+        'body': message.body,
+        'username': message.username,
+        'created_at': message.created_at,
+        'updated_at': message.updated_at
+    }), 201
 
-    for i in range(20):
-        message = Message(
-            body=fake.sentence(),
-            username=rc(usernames),
-        )
-        messages.append(message)
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+    message.body = data['body']
+    db.session.commit()
+    return jsonify({
+        'id': message.id,
+        'body': message.body,
+        'username': message.username,
+        'created_at': message.created_at,
+        'updated_at': message.updated_at
+    })
 
-    db.session.add_all(messages)
-    db.session.commit()        
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return jsonify({'message': 'Message deleted successfully'}), 200
 
 if __name__ == '__main__':
-    with app.app_context():
-        make_messages()
+    app.run(port=5555)
